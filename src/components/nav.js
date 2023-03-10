8dcb0052308ffd63145cf416a104d872be00a980
doodlehub <chaochen@live.com>
2023-03-10 15:23:48 -0500

    feature: set up nav bar

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,1 +1,17 @@
-export const Nav = () => <div>I am the navigation component.</div>;
+export const Nav = () => (
+  <nav>
+    <div className="h-16 bg-gradient-to-l from-black via-gray-900 to-gray-800">
+      <div className="flex h-full items-center justify-end gap-8 pr-10 text-lg font-medium text-teal-600">
+        <a className="hover:text-white" href="#">
+          Home
+        </a>
+        <a className="hover:text-white" href="#">
+          Portfolio
+        </a>
+        <a className="hover:text-white" href="#">
+          Blog
+        </a>
+      </div>
+    </div>
+  </nav>
+);
